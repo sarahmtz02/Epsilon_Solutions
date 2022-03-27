4b58c51cfab1ee0a8ab7eb9a207ca0fe2033ec93
Jaime-prog <A00571842@tec.mx>
2022-03-26 22:38:09 -0600

    Creacion de Middlewares para pagina 404 y  Login de pagina

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+//motor de plantilla
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+app.use((req, res, next) => {
+    res.render("404");
+})
+
+app.listen(port, () => {
+    console.log('Servidor a su servicio en el puerto ', port)
+})
