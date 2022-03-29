7c680d8f8c2484ffa0a215d33d2217a85e3b0097
MawLr <mano_med_rod@hotmail.com>
2022-03-29 09:15:38 -0600

    Modificaciones Frontend

--- Backend/app.js
+++ Backend/app.js
@@ -16,6 +16,8 @@ app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({
