c3f9fd4b50fa99c65ad1e4dc1bf15a884e25af79
MawLr <mano_med_rod@hotmail.com>
2022-04-01 00:11:45 -0600

    Requiere autenticación para acceder a vistas

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,6 @@ const csrfProtection = csrf();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
-//const rutas_users = require('./routes/user.routes');
 const rutas_empleados = require('./routes/empleados.routes')
 const path = require('path');
 
@@ -21,15 +20,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+
 app.use(session({
     secret: 'Prueba de Cookies', 
     resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
 }));
 
-//app.use(csrfProtection); 
+app.use(csrfProtection); 
 
-//app.use('/users', rutas_users);
+app.use((request, response, next) => {
+    response.locals.csrfToken = request.csrfToken();
+    next();
+});
+
 app.use('/empleados', rutas_empleados);
 
 //Middleware
