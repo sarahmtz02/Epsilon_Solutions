e5575c57a3e02565c419e8ad57d79464f2e73168
Jaime-prog <A00571842@tec.mx>
2022-05-03 18:20:05 -0500

    Conexion a base de datos en servidor

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -1,7 +1,7 @@
-//const mysql = require('mysql2');
+const mysql = require('mysql2');
 
 // Para revertir y hacer pruebas sólo hay que conectarse a la DB local con el código siguiente: (Las tablas van en minuscula!)
-
+/*
 const { createPool } = require("mysql2");
 const pool = createPool({
     host: 'bpnzdg3qreqalbxfqwow-mysql.services.clever-cloud.com',
@@ -13,10 +13,11 @@ const pool = createPool({
 });
 
 module.exports = pool.promise();
+*/
 
 // DB en CleverCloud, las tablas no van en minuscula!
 
-/*const { createPool } = require("mysql2");
+const { createPool } = require("mysql2");
 
 const pool = createPool({
     host: 'localhost',
@@ -25,6 +26,6 @@ const pool = createPool({
     port: 3306,
     database: 'SIRE',
     timezone: 'Z'
-});*/
+});
 
 module.exports = pool.promise();
