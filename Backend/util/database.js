93039e1fd6db298cc5327d60c9004f2bab408eac
Castro J. Jorge <74395532+jorgeCastro-CVJJ@users.noreply.github.com>
2022-05-04 12:16:00 -0500

    Https port test

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -1,7 +1,7 @@
 //const mysql = require('mysql2');
 
 // Para revertir y hacer pruebas sólo hay que conectarse a la DB local con el código siguiente: (Las tablas van en minuscula!)
-
+/*
 const { createPool } = require("mysql2");
 const pool = createPool({
     host: 'bpnzdg3qreqalbxfqwow-mysql.services.clever-cloud.com',
@@ -13,21 +13,21 @@ const pool = createPool({
 });
 
 module.exports = pool.promise();
-
+*/
 
 // DB en CleverCloud, las tablas no van en minuscula!
 
-/*const { createPool } = require("mysql2");
+const { createPool } = require("mysql2");
 
 const pool = createPool({
     host: 'localhost',
     user: 'root',
     password: 'Epsil0n*',
-    port: 3306,
+    port: 80,
     database: 'SIRE',
     timezone: 'Z'
 });
 
 
 module.exports = pool.promise();
-*/
+
